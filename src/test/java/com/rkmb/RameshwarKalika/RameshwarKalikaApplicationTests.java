51f1a9b857e28dd831bc28182b9df8e6373c1cd7
Suraj Solanki <suraj.solanki.750@gmail.com>
2023-10-10 14:07:54 +0530

    Adding local project to git repository

---
+++ src/test/java/com/rkmb/RameshwarKalika/RameshwarKalikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkmb.RameshwarKalika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RameshwarKalikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
