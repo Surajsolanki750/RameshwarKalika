301f1be2e51a2fbe99b0e8ec56c916718c77a411
Suraj Solanki <suraj.solanki.750@gmail.com>
2023-10-12 17:43:44 +0530

    Custom Exception class added

---
+++ src/main/java/com/rkmb/RameshwarKalika/Exception/ResourceNotFoundExcpetion.java
@@ -0,0 +1,11 @@
+package com.rkmb.RameshwarKalika.Exception;
+
+public class ResourceNotFoundExcpetion extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundExcpetion(String message) {
+		super(message);
+	}
+	
+}
