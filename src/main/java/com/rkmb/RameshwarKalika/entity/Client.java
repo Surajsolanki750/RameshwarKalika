ddf8440dbc61f722b2688fc7c723dd01b44546a7
Suraj Solanki <suraj.solanki.750@gmail.com>
2023-10-13 17:58:46 +0530

    Added constructor in entity class

--- src/main/java/com/rkmb/RameshwarKalika/entity/Client.java
+++ src/main/java/com/rkmb/RameshwarKalika/entity/Client.java
@@ -26,41 +26,67 @@ public class Client {
 	private Long clientIncome;
 	@Column(name = "client_caste")
 	private String clientCaste;
+
 	public Integer getClientId() {
 		return clientId;
 	}
+
 	public void setClientId(Integer clientId) {
 		this.clientId = clientId;
 	}
+
 	public String getClientFullName() {
 		return clientFullName;
 	}
+
 	public void setClientFullName(String clientFullName) {
 		this.clientFullName = clientFullName;
 	}
+
 	public Date getClientDOB() {
 		return clientDOB;
 	}
+
 	public void setClientDOB(Date clientDOB) {
 		this.clientDOB = clientDOB;
 	}
+
 	public String getClientJobDetails() {
 		return clientJobDetails;
 	}
+
 	public void setClientJobDetails(String clientJobDetails) {
 		this.clientJobDetails = clientJobDetails;
 	}
+
 	public Long getClientIncome() {
 		return clientIncome;
 	}
+
 	public void setClientIncome(Long clientIncome) {
 		this.clientIncome = clientIncome;
 	}
+
 	public String getClientCaste() {
 		return clientCaste;
 	}
+
 	public void setClientCaste(String clientCaste) {
 		this.clientCaste = clientCaste;
 	}
-	
+
+	public Client(Integer clientId, String clientFullName, Date clientDOB, String clientJobDetails, Long clientIncome,
+			String clientCaste) {
+		this.clientId = clientId;
+		this.clientFullName = clientFullName;
+		this.clientDOB = clientDOB;
+		this.clientJobDetails = clientJobDetails;
+		this.clientIncome = clientIncome;
+		this.clientCaste = clientCaste;
+	}
+
+	public Client() {
+
+	}
+
 }
