25bb435985163ba1cd80f25d654cfd4803d68635
Suraj Solanki <suraj.solanki.750@gmail.com>
2023-10-13 13:20:24 +0530

    Rest Controller added

---
+++ src/main/java/com/rkmb/RameshwarKalika/controller/ClientController.java
@@ -0,0 +1,10 @@
+package com.rkmb.RameshwarKalika.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/client")
+public class ClientController {
+
+}
