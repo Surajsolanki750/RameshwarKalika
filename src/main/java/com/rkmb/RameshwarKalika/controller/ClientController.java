f29f6b853ff54899ed5cb502ef7824ff1f20c243
Suraj Solanki <suraj.solanki.750@gmail.com>
2023-10-18 17:01:30 +0530

    delete mapping added

--- src/main/java/com/rkmb/RameshwarKalika/controller/ClientController.java
+++ src/main/java/com/rkmb/RameshwarKalika/controller/ClientController.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rkmb.RameshwarKalika.entity.Client;
@@ -44,15 +46,25 @@ public class ClientController {
 		service.saveAll(list);
 		return "All the Data Stored Successfully";
 	}
-	
+
 	@PutMapping("update/{id}")
-	public ResponseEntity<String> updateClientDetails(@PathVariable Integer id, @RequestBody Client client){
-		if(id!=null) {
+	public ResponseEntity<String> updateClientDetails(@PathVariable Integer id, @RequestBody Client client) {
+		if (id != null) {
 			service.updateClientDetails(id, client);
 			return ResponseEntity.ok("Record updated");
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND)
+					.body("The entered id " + id + " is not in the database please check once again");
 		}
-		else {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("The entered id "+ id +" is not in the database please check once again");
+	}
+
+	@DeleteMapping("delete/{id}")
+	public String deleteClientData(@RequestParam Integer id) {
+		if(id!=null) {
+			service.deleteClientById(id);
+			return "data deleted";
+		}else {
+			return "There is no data for given id = " +id+" in the database";
 		}
 	}
 
