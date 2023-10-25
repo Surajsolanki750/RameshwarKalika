734e25c88b5d20f1d9d426c273017f5a60349313
Suraj Solanki <suraj.solanki.750@gmail.com>
2023-10-25 17:58:23 +0530

    Removed extra spaced & unncessary imports from swagger

--- src/main/java/com/rkmb/RameshwarKalika/swagger/SwaggerConfig.java
+++ src/main/java/com/rkmb/RameshwarKalika/swagger/SwaggerConfig.java
@@ -6,8 +6,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.rkmb.RameshwarKalika.Exception.ResourceNotFoundExcpetion;
-
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -18,12 +16,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableWebMvc
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig implements WebMvcConfigurer{
+public class SwaggerConfig implements WebMvcConfigurer {
 
 	private final String serviceTitle = "Rameshwar Kalika Service";
 	private final String serviceDescription = "This document contains APIS of client of Rameshwar Kalika";
 	private final String version = "1.1.0";
-	
+
 	@Bean
 	public Docket configApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
@@ -34,10 +32,10 @@ public class SwaggerConfig implements WebMvcConfigurer{
 	private ApiInfo apiEndPointInfo() {
 		return new ApiInfoBuilder().title(serviceTitle).description(serviceDescription).version(version).build();
 	}
-	
+
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/Resources/");
-		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/Resources/webjars/");
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 
 }
